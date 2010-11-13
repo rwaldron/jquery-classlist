eef9e2dcc384d9fd0e0f04bf5e949a4d35be1bad
Rick Waldron <waldron.rick@gmail.com>
2010-11-12 23:36:42 -0500

    Perf tweak; native classList.add()

--- jquery.classlist.js
+++ jquery.classlist.js
@@ -7,8 +7,9 @@
  */
 (function (jQuery, undefined) {
 
-  var slice = slice || Array.prototype.slice, 
-      hasClassList = jQuery.support.classList || !!$('<div/>')[0].classList;
+  var slice = slice || Array.prototype.slice;
+
+  jQuery.support.classList = !!$('<div/>')[0].classList;
   
   
   jQuery.fn.extend({
@@ -18,14 +19,14 @@
     classlist: function ( value ) {
       
       var elem = this[0], iter = 0, 
-          classNames, className, listLength;
+          classNames, className, len;
           
       //  Getter logic
       if ( !value ) {
         // Native classList is an array-like object; for normalization 
         // with non-native implementations, we return arrays 
         // This approach has proven significantly faster
-        classNames = hasClassList ? 
+        classNames = jQuery.support.classList ? 
                       elem.classList.toString() :
                       elem.className;
         
@@ -34,9 +35,9 @@
       
       if ( jQuery.type(value) === "array" ) {
         
-        listLength  = this.length;
+        len  = this.length;
         
-        while ( listLength-- ) {
+        while ( len-- ) {
           elem = this[iter++];
           
           if ( elem.nodeType === 1 ) {
@@ -45,13 +46,18 @@
               elem.className = value.join(" ");
 
             } else {
-            
+              
               className = " " + elem.className + " ",
                 setClass = elem.className;
 
               for ( var c = 0, cl = value.length; c < cl; c++ ) {
-                if ( className.indexOf( " " + value[c] + " " ) < 0 ) {
-                  elem.className += " " + value[c];
+                
+                if ( jQuery.support.classList ) {
+                  elem.classList.add(value[c]);
+                } else {
+                  if ( className.indexOf( " " + value[c] + " " ) < 0 ) {
+                    elem.className += " " + value[c];
+                  }
                 }
               }
             }
@@ -60,64 +66,6 @@
 
         return this;
       }
-      
-      // Everything below this comment is superfluous
-      var arg, args, apiFn, list, 
-          fixMethods = {
-            "contains" : "has"
-          },
-          noids = {
-            "null": true, 
-            "undefined": true,
-            "false": true
-          };
-      
-
-      arguments.length && ( arg  = Array.prototype.join.call(arguments) );
-      
-      args = arg && arg.match(/(\w+)/gi);
-      
-      if ( args && args.length >= 2 && !noids[ args[1] ] ) {
-        
-        // compile a possible jQuery method
-        apiFn = ( fixMethods[ args[0] ] ? fixMethods[ args[0] ] : args[0] ) + "Class";
-
-        // Check if we can shortcut to an existing method
-        if ( jQuery.fn[ apiFn ] ) {
-          
-          if ( !hasClassList ) {
-            // Uses jQuery addClass, removeClass and toggleClass in single and multi mode 
-            // as fallback to native classList methods
-            // Returns as documented for these methods
-            return jQuery(this)[ apiFn ]( args.slice(1).join(" ") );
-          }
-          
-          //  Use native classList.contains() for performance
-          if ( args[0] === 'contains' ) {
-            return elem.classList.contains( args[1] );
-          } 
-          
-          //  Use native classlist.add,remove,toggle for performance
-          list  = args.slice(1);
-          
-          this.each(function () {
-            for ( var i = 0, len = list.length; i < len; i++ ) {
-              this.classList[ args[0] ]( list[i] );
-            }
-          });
-
-        } else {
-
-          //  Should only resolve to this when item() is called;
-          //  Return only for the first matched element
-          return hasClassList ? 
-                 elem.classList.item(+args[1]) : 
-                 jQuery(elem).classlist()[ +args[1] ];
-                 // might be faster: elem.className.split(" ")[ +args[1] ]
-        }                 
-      }
-      
-      return jQuery(elem).classlist();
     }
   });
 })(jQuery);
