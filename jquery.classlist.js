377b5a17cb08995a98d6fc1f9ebe331f58a3c591
Rick Waldron <waldron.rick@gmail.com>
2010-11-12 23:38:03 -0500

    Perf tweak; native classList.add()

--- jquery.classlist.js
+++ jquery.classlist.js
@@ -66,6 +66,64 @@
 
         return this;
       }
+      
+      // Everything below this comment is superfluous
+      var arg, args, apiFn, list, 
+          fixMethods = {
+            "contains" : "has"
+          },
+          noids = {
+            "null": true, 
+            "undefined": true,
+            "false": true
+          };
+      
+
+      arguments.length && ( arg  = Array.prototype.join.call(arguments) );
+      
+      args = arg && arg.match(/(\w+)/gi);
+      
+      if ( args && args.length >= 2 && !noids[ args[1] ] ) {
+        
+        // compile a possible jQuery method
+        apiFn = ( fixMethods[ args[0] ] ? fixMethods[ args[0] ] : args[0] ) + "Class";
+
+        // Check if we can shortcut to an existing method
+        if ( jQuery.fn[ apiFn ] ) {
+          
+          if ( !hasClassList ) {
+            // Uses jQuery addClass, removeClass and toggleClass in single and multi mode 
+            // as fallback to native classList methods
+            // Returns as documented for these methods
+            return jQuery(this)[ apiFn ]( args.slice(1).join(" ") );
+          }
+          
+          //  Use native classList.contains() for performance
+          if ( args[0] === 'contains' ) {
+            return elem.classList.contains( args[1] );
+          } 
+          
+          //  Use native classlist.add,remove,toggle for performance
+          list  = args.slice(1);
+          
+          this.each(function () {
+            for ( var i = 0, len = list.length; i < len; i++ ) {
+              this.classList[ args[0] ]( list[i] );
+            }
+          });
+
+        } else {
+
+          //  Should only resolve to this when item() is called;
+          //  Return only for the first matched element
+          return hasClassList ? 
+                 elem.classList.item(+args[1]) : 
+                 jQuery(elem).classlist()[ +args[1] ];
+                 // might be faster: elem.className.split(" ")[ +args[1] ]
+        }                 
+      }
+      
+      return jQuery(elem).classlist();
     }
   });
 })(jQuery);
